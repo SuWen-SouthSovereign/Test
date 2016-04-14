6e4877780d5460f6eb7336ee5d2b1ce3b5b2215c
suwen@163.com <suze@suwendemacbook-air.local>
2016-04-14 19:35:53 +0800

    创建一个Person类

---
+++ demo/demo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  demo
+//
+//  Created by 蘇崢 on 16/4/14.
+//  Copyright © 2016年 蘇崢. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ demo/demo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  demo
+//
+//  Created by 蘇崢 on 16/4/14.
+//  Copyright © 2016年 蘇崢. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
